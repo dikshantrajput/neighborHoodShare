62abecbfdf088f9a375009d0fc1bd85c4a3e0784
Dikshant rajput <dikshantraj2001@gmail.com>
2024-05-04 19:55:08 +0530

    added web analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import "../app.css";
     import Header2 from "../components/Header2.svelte";
     import Footer2 from "../components/Footer2.svelte";
-    import { Toaster } from 'svelte-sonner'
+    import { Toaster } from 'svelte-sonner';
 </script>
 
 <div class="flex flex-col min-h-screen dark-bg">

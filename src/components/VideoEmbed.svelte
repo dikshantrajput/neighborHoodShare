aa30aa2b922e59d73bfef2d03c424f3742be39c2
Dikshant rajput <dikshantraj2001@gmail.com>
2024-05-05 12:51:34 +0530

    fix styling

--- src/components/VideoEmbed.svelte
+++ src/components/VideoEmbed.svelte
@@ -1,17 +1,35 @@
 <script>
+    import { fade } from "svelte/transition";
+
     export let videoId; // Declare a prop to pass the YouTube video ID
   </script>
   
-  <div class="mx-auto max-w-[800px] relative aspect-video m-10">
-    <iframe
-      title="YouTube Video"
-      class="absolute top-0 left-0 w-full h-full"
-      src="https://www.youtube.com/embed/{videoId}"
-      frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    />
-  </div>
+
+  <section class="bg-gray-900 py-20" id="features">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="lg:text-center">
+            <h2
+                class="text-base font-semibold text-indigo-600 uppercase tracking-wide"
+                in:fade={{ delay: 100, duration: 500 }}
+            >
+                NeighborHoodShare in action
+            </h2>
+        </div>
+        <div class="mx-auto max-w-[800px] relative aspect-video mt-5">
+            <iframe
+              title="YouTube Video"
+              class="absolute top-0 left-0 w-full h-full"
+              src="https://www.youtube.com/embed/{videoId}"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+    </div>
+</section>
+
+
+  
   
   <style>
     .aspect-video {
